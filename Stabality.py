e9d9d5d520e1253656c50345a2115f554f17520b
Ahmet <121121504+MyCodingSpace5@users.noreply.github.com>
2022-12-26 17:02:23 +0000

    Create Stabality.py

---
+++ Stabality.py
@@ -0,0 +1,9 @@
+import requests
+import urllib.request as ur
+
+imagegen = input("What image you wanna generate? Enter the prompt here")
+url = f"https://api.newnative.ai/stable-diffusion?prompt={imagegen}"
+response = requests.request("GET", url)
+data = response.json()
+print(data["image_url"])
+
